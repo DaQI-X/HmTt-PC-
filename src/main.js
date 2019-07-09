8e43724701498e1a061b768892da24268a26d6ca
Mr.Qi <123123@qk.com>
2019-07-09 17:06:46 +0800

    配置router和element ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 导入element ui 和 css样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入 router 路由模块
+import router from './router'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+// 路由组件
+// 导入Vue
+import Vue from 'vue'
+// 导入 路由
+import VueRouter from 'vue-router'
+// 匹配路由
+Vue.Vue.use(VueRouter)
+// 配置Vue
+Vue.config.productionTip = false
+
+// 配置路由规则
+const router = new VueRouter({
+  // 配置路由组件
+  routes: [
+
+  ]
+})
+
+// ES6 导出语法
+export default router
