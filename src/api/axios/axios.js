7b2b068baa7b50190955937c45f186ee31779b13
Mr.Qi <123123@qk.com>
2019-07-17 20:57:48 +0800

    更新筛选、删除、编辑跳转

--- src/api/axios/axios.js
+++ src/api/axios/axios.js
@@ -1,25 +1,37 @@
 // axios 模块 --
 // 导入 axios
 import axios from 'axios'
+// 导入 JSONBig
+import JSONBig from 'json-bigint'
 // 封装 axios
 const instance = axios.create({
   // 配置axios url
-  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0'
+  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0',
   // 请求头不能放在封装的axios里，
-  // 如果没有token，会一直加载页面--
+  // 如果没有tokenF，会一直加载页面--
+
+  // 对于 data 数据为 null 做出的严谨判断
+  transformResponse: [(data) => {
+    // 处理格式
+    if (data) {
+      return JSONBig.parse(data)
+    }
+    return data
+  }]
 })
+
 // axios-请求拦截器
 // 在请求之前，先获取token如果有正常访问，如果没有进行ajax请求，实现登录获取token
 
 // 添加 --请求拦截器--
 // 在请求之前先获取token，有的话继续执行，没有则重新登录请求token
-axios.interceptors.request.use(function (config) {
+instance.interceptors.request.use(function (config) {
   // 在发送请求之前先获取token，
   const users = window.sessionStorage.getItem('hm73-toutiao')
   if (users) {
     // 如果是 true 拿到 token 修改config继续做校验
     config.headers = {
-      Authorization: 'Bearer' + JSON.parse(users).token
+      Authorization: 'Bearer ' + JSON.parse(users).token
     }
   }
   // 如果没有，则继续发送ajax请求数据
@@ -31,14 +43,15 @@ axios.interceptors.request.use(function (config) {
 
 // 添加响应拦截器--
 // 如果token失效或者关闭浏览器，则必须重新登录请求token
-axios.interceptors.response.use(function (response) {
+instance.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response
 }, function (error) {
   // 如果 响应数据的 状态码为 401 ，那么就重新进行登录
-  if (error.response.status === 401) {
+  // 严谨判断，如果有 响应 才会去获取 status
+  if (error.response && error.response.status === 401) {
     // 跳转登录页，进行重新登录
-    location.hash = '#/login'
+    location.hash = '/login'
   }
 
   return Promise.reject(error)
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="articlecont">
+    <!-- 头部分 -->
     <el-card class="box-card">
       <!-- 面包屑导航 -->
       <div slot="header">
-          <!-- 插槽 -->
+        <!-- 插槽 -->
         <my-bread>内容管理</my-bread>
       </div>
 
@@ -33,7 +34,9 @@
         <el-form-item label="时间：">
           <!-- <span class="demonstration">默认</span> -->
           <el-date-picker
+            value-format="yyyy-MM-dd"
             v-model="dateValues"
+            @change="changeDate"
             type="daterange"
             range-separator="至"
             start-placeholder="开始日期"
@@ -41,48 +44,154 @@
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" icon="el-icon-search">筛选</el-button>
+          <el-button type="primary" @click="search()">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
 
-    <!--  -->
-    <el-card>
-
+    <!-- 底部分 -->
+    <el-card class="box-card">
+      <div slot="header">
+        根据筛选条件共查询到
+        <b>{{total}}</b>条结果
+      </div>
+      <el-table :data="articles" style="width: 100%">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image lazy :src="scope.row.cover.images[0]" style="width:100px;height:75px">
+              <div slot="error" class="image-slot">
+                <img src="../../assets/images/error.gif" width="100" height="75" />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column prop="title" label="标题"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1">待审核</el-tag>
+            <el-tag type="success" v-if="scope.row.status===2">审核通过</el-tag>
+            <el-tag type="warning" v-if="scope.row.status===3">审核失败</el-tag>
+            <el-tag type="danger" v-if="scope.row.status===4">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="pubdate" label="发布时间"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button plain type="primary" icon="el-icon-edit" @click="edit(scope.row.id)" circle></el-button>
+            <el-button plain type="danger" icon="el-icon-delete" @click="del(scope.row.id)" circle></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
+    <div class="box">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        @current-change="changePager"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
       // 提交给后台的筛选条件  传参
       // 数据默认是''还是null的区别,如果是null将不会发送字段
       reqParams: {
+        page: 1,
+        per_page: 20,
         status: null,
         channel_id: null,
         begin_pubdate: null,
         end_pubdate: null
       },
+      // 文章内容数据
+      articles: [],
       // 默认频道数据
       channelOptions: [],
       // 日期控件的数据
-      dateValues: []
+      dateValues: [],
+      // 总条数
+      total: 0
     }
   },
   // 获取频道数据--
   created () {
-    // 获取频道数据--
-    this.getChanneLo()
+    // 获取频道数据
+    this.getChannel()
+    // 获取文章内容列表
+    this.getArticles()
   },
   methods: {
-    // 获取频道数据--
-    async getChanneLo () {
-      // 多层解构
-      var { data: { data } } = await this.$http.get('channels')
+    // 异步发送ajax请求，获取频道数据
+    async getChannel () {
+      const {
+        data: { data }
+      } = await this.$http.get('channels', { params: this.reqParams })
       this.channelOptions = data.channels
+    },
+    // 获取文章内容数据
+    async getArticles () {
+      const {
+        data: { data }
+      } = await this.$http.get('articles', { params: this.reqParams })
+      // 获取数据文章列表内容
+      this.articles = data.results
+      // 获取总条数
+      this.total = data.total_count
+    },
+    // 删除当前文章内容的数据
+    del (id) {
+      this.$confirm('此操作将永久删除该文章, 是否继续?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(async () => {
+          // 发送ajax请求
+          await this.$http.delete(`articles/${id}`)
+          // 删除成功提示信息
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    // 编辑当前文章
+    edit (id) {
+      // 路径传参用 query ；键值对传参用 params
+      // this.$router.push({ path: '/publish', query: { id } }) // 路径传参
+      this.$router.push({path:'/publish',params:{id:id}})
+    },
+    // 点击搜索的时候--重新获取列表数据
+    changeDate (values) {
+      this.reqParams.begin_pubdate = values[0]
+      this.reqParams.end_pubdate = values[1]
+    },
+    // 点击搜索按钮
+    search () {
+      this.reqParams.page = 1
+      this.getArticles()
+    },
+    // 页面数据内容监听
+    changePager (newPage) {
+      // newPage 当前点击的按钮的页码
+      // 更新提交给后台的参数
+      this.reqParams.page = newPage
+      // 获取当前数据
+      this.getArticles()
     }
   }
 }
@@ -94,5 +203,10 @@ export default {
   .el-form-item {
     padding-top: 5px;
   }
+}
+//分页
+.box {
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
